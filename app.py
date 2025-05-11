b4c2a9f9b6360a3d2a96f7c5985382d1910eca08
antoancms <antoandobratova@outlook.com>
2025-05-12 01:34:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def preview(id):
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
